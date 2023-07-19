9a02917accd6ea5c46fcf741fc98500d66297e8e
ミーシャ-チェリー <72266338+bublickkk@users.noreply.github.com>
2023-07-19 17:16:53 +0300

    Updated test branch

---
+++ new/test/src/js/lang.js
@@ -0,0 +1,58 @@
+const LangArray = {
+    "title" : {
+        "ru":"Домашняя",
+        "en":"Home",
+    },
+    "about_us" : {
+        "ru": "О нас",
+        "en": "About us",
+    },
+    "projects" : {
+        "ru": "Проекты",
+        "en": "Projects",
+    },
+    "team" : {
+        "ru": "Команда",
+        "en": "Team",
+    },
+    "contacts" : {
+        "ru": "Контакты",
+        "en": "Contacts",
+    },
+    "about_us_desc" : {
+        "ru": "Мы, Reverse, разрабатываем различный софт, а также игры. В данный момент у нас ведется разработка нескольких проектов. Следить за всеми процессами вы можете в наших соц. сетях во вкладке #contacts",
+        "en": "We, Reverse, develop various software as well as games. At the moment we are developing several projects. You can follow all the processes in our social networks in the #contacts tab.",
+    },
+    "storm_desc" : {
+        "ru": "тест",
+        "en": "test",
+    },
+    "launcher_desc" : {
+        "ru": "l_тест",
+        "en": "L_test",
+    },
+    "teamdesc-bblckk" : {
+        "ru": "Лид дев & владелец 'reverse' ",
+        "en": "Lead Dev & 'reverse' owner",
+    },
+    "teamdesc-geniy" : {
+        "ru": "Дев & совладелец 'reverse'",
+        "en": "Dev & 'reverse' co-owner",
+    },
+    "teamdesc-dead_bebrax" : {
+        "ru": "Дизайнер",
+        "en": "Designer",
+    },
+    "teamdesc-steven56" : {
+        "ru": "Дев & Дизайнер",
+        "en": "Dev & Designer",
+    },
+    "teamdesc-bebreef" : {
+        "ru": "Дев",
+        "en": "Dev",
+    },
+    "teamdesc-okokus" : {
+        "ru": "Дев & владелец паблика 'ОКОКА'",
+        "en": "Dev",
+    },
+}
