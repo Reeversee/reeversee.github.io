f96ab68fab219c58f330c00b7efb15882df97bc1
ミーシャ-チェリー <72266338+bublickkk@users.noreply.github.com>
2023-07-19 20:13:34 +0300

    Added Ukraine language

--- new/test/src/js/lang.js
+++ new/test/src/js/lang.js
@@ -1,62 +1,77 @@
 const LangArray = {
     "title" : {
         "ru":"Домашняя",
+        "ua":"Домашня",
         "en":"Home",
     },
     "about_us" : {
         "ru": "О нас",
+        "ua": "Про нас",
         "en": "About us",
     },
     "projects" : {
         "ru": "Проекты",
+        "ua": "Проєкти",
         "en": "Projects",
     },
     "team" : {
         "ru": "Команда",
+        "ua": "Команда",
         "en": "Team",
     },
     "contacts" : {
         "ru": "Контакты",
+        "ua": "Контакти",
         "en": "Contacts",
     },
     "about_us_desc" : {
         "ru": "Мы, Reverse, разрабатываем различный софт, а также игры. В данный момент у нас ведется разработка нескольких проектов. Следить за всеми процессами вы можете в наших соц. сетях во вкладке #contacts",
+        "ua": "Ми, Reverse, розробляємо різний софт, а також ігри. Наразі у нас триває розробка кількох проєктів. Стежити за всіма процесами ви можете в наших соц. мережах у вкладці #contacts",
         "en": "We, Reverse, develop various software as well as games. At the moment we are developing several projects. You can follow all the processes in our social networks in the #contacts tab.",
     },
     "storm_desc" : {
         "ru": "Мультиплеерный шутер разработанный на игровом движке Unreal Engine 5.2",
+        "ua": "Мультиплеєрний шутер розроблений на ігровому рушії Unreal Engine 5.2",
         "en": "Multiplayer shooter developed on Unreal Engine 5.2 engine",
     },
     "launcher_desc" : {
         "ru": "Наш собственный лаунчер созданный для удобного скачивания наших проектов.",
+        "ua": "Наш власний лаунчер створений для зручного скачування наших проєктів.",
         "en": "Our own Launcher created for easy downloading of our projects.",
     },
     "teamdesc-bblckk" : {
-        "ru": "Лид дев & владелец 'reverse' ",
-        "en": "Lead Dev & 'reverse' owner",
+        "ru": "Ведущий разработчик & владелец 'Reverse' ",
+        "ua": "Провідний розробник & власник 'Reverse'",
+        "en": "Lead Dev & 'Reverse' owner",
     },
     "teamdesc-geniy" : {
-        "ru": "Дев & совладелец 'reverse'",
-        "en": "Dev & 'reverse' co-owner",
+        "ru": "Разработчик & совладелец 'Reverse'",
+        "ua": "Розробник & співвласник 'Reverse'",
+        "en": "Dev & 'Reverse' co-owner",
     },
     "teamdesc-dead_bebrax" : {
         "ru": "Дизайнер",
+        "ua": "Дизайнер",
         "en": "Designer",
     },
     "teamdesc-steven56" : {
-        "ru": "Дев & Дизайнер",
+        "ru": "Разработчик & Дизайнер",
+        "ua": "Розробник & Дизайнер",
         "en": "Dev & Designer",
     },
     "teamdesc-bebreef" : {
-        "ru": "Дев",
+        "ru": "Разработчик",
+        "ua": "Розробник",
         "en": "Dev",
     },
     "teamdesc-okokus" : {
-        "ru": "Дев & владелец паблика 'ОКОКА'",
+        "ru": "Разработчик & владелец паблика 'ОКОКА'",
+        "ua": "Розробник & власник пабліка 'ОКОКА'",
         "en": "Dev",
     },
     "sourcecode" : {
         "ru": "Исходный код доступен на GitHub",
+        "ua": "Вихідний код доступний на GitHub",
         "en": "Source code available on GitHub",
     },
 }
