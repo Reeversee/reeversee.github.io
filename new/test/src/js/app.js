e0ceb8d1f051b6aaf7ed99cc2d1df81524ac7c67
ミーシャ-チェリー <72266338+bublickkk@users.noreply.github.com>
2023-07-27 12:45:21 +0300

    Added Czech & Belarusian languages

--- new/test/src/js/app.js
+++ new/test/src/js/app.js
@@ -1,5 +1,5 @@
 const select = document.querySelector('select');
-const allLang = ['en', 'ru', 'ua', 'by', 'cz'];
+const allLang = ['en', 'ru', 'ua', 'be', 'cz'];
 
 select.addEventListener('change', changeURLLanguage);
 
--- new/test/src/js/lang.js
+++ new/test/src/js/lang.js
@@ -1,81 +1,111 @@
 const LangArray = {
+    // page title
     "title" : {
         "ru":"Домашняя",
         "ua":"Домашня",
         "en":"Home",
         "cz":"Domácí",
+        "be":"Хатні",
     },
+    // descriptions
     "about_us" : {
         "ru": "О нас",
         "ua": "Про нас",
         "en": "About us",
         "cz": "O nás",
+        "be": "Пра нас",
     },
     "projects" : {
         "ru": "Проекты",
         "ua": "Проєкти",
         "en": "Projects",
         "cz": "Projekty",
+        "be": "Праект",
     },
     "team" : {
         "ru": "Команда",
         "ua": "Команда",
         "en": "Team",
         "cz": "Tým",
+        "be": "Каманда",
     },
     "contacts" : {
         "ru": "Контакты",
         "ua": "Контакти",
         "en": "Contacts",
+        "cz": "Kontakty",
+        "be": "Кантакт",
     },
     "about_us_desc" : {
         "ru": "Мы, Reverse, разрабатываем различный софт, а также игры. В данный момент у нас ведется разработка нескольких проектов. Следить за всеми процессами вы можете в наших соц. сетях во вкладке #contacts",
         "ua": "Ми, Reverse, розробляємо різний софт, а також ігри. Наразі у нас триває розробка кількох проєктів. Стежити за всіма процесами ви можете в наших соц. мережах у вкладці #contacts",
         "en": "We, Reverse, develop various software as well as games. At the moment we are developing several projects. You can follow all the processes in our social networks in the #contacts tab.",
+        "cz": "My, Reverse, vyvíjíme různý software i hry. V současné době vyvíjíme několik projektů. Všechny procesy můžete sledovat na našich sociálních sítích v záložce #kontakty.",
+        "be": "Мы, Reverse, распрацоўваем розны софт, а таксама гульні. У дадзены момант у нас вядзецца распрацоўка некалькіх праектаў. Сачыць за ўсімі працэсамі вы можаце ў нашых соц. сетках ва ўкладцы #contacts",
     },
     "storm_desc" : {
         "ru": "Мультиплеерный шутер разработанный на игровом движке Unreal Engine 5.2",
         "ua": "Мультиплеєрний шутер розроблений на ігровому рушії Unreal Engine 5.2",
         "en": "Multiplayer shooter developed on Unreal Engine 5.2 engine",
+        "cz": "Střílečka pro více hráčů vyvinutá na enginu Unreal Engine 5.2",
+        "be": "Мультыплэерны шутэр распрацаваны на гульнявым рухавічку Unreal Engine 5.2",
     },
     "launcher_desc" : {
         "ru": "Наш собственный лаунчер созданный для удобного скачивания наших проектов.",
         "ua": "Наш власний лаунчер створений для зручного скачування наших проєктів.",
         "en": "Our own Launcher created for easy downloading of our projects.",
+        "cz": "Náš vlastní spouštěč vytvořený pro snadné stahování našich projektů.",
+        "be": "Наш уласны лаунчер створаны для зручнага запампоўкі нашых праектаў.",
     },
+    // team descriptions
     "teamdesc-bblckk" : {
-        "ru": "Ведущий разработчик & владелец 'Reverse' ",
+        "ru": "Ведущий разработчик & владелец 'Reverse'",
         "ua": "Провідний розробник & власник 'Reverse'",
         "en": "Lead Dev & 'Reverse' owner",
+        "cz": "Lead Dev & 'Reverse' owner",
+        "be": "Вядучы распрацоўшчык & уладальнік 'Reverse'",
     },
     "teamdesc-geniy" : {
         "ru": "Разработчик & совладелец 'Reverse'",
         "ua": "Розробник & співвласник 'Reverse'",
         "en": "Dev & 'Reverse' co-owner",
+        "cz": "Dev & 'Reverse' co-owner",
+        "be": "Распрацоўшчык & саўладальнік 'Reverse'",
     },
     "teamdesc-dead_bebrax" : {
         "ru": "Дизайнер",
         "ua": "Дизайнер",
         "en": "Designer",
+        "cz": "Designer",
+        "be": "Дызайнер",
     },
     "teamdesc-steven56" : {
         "ru": "Разработчик & Дизайнер",
         "ua": "Розробник & Дизайнер",
         "en": "Dev & Designer",
+        "cz": "Dev & Designer",
+        "be": "Распрацоўшчык & Дызайнер",
     },
     "teamdesc-bebreef" : {
         "ru": "Разработчик",
         "ua": "Розробник",
         "en": "Dev",
+        "cz": "Dev",
+        "be": "Распрацоўшчык",
     },
     "teamdesc-okokus" : {
         "ru": "Разработчик & владелец паблика 'ОКОКА'",
         "ua": "Розробник & власник пабліка 'ОКОКА'",
         "en": "Dev",
+        "cz": "Dev",
+        "be": "Распрацоўшчык & уладальнік пабліка 'ОКОКА'",
     },
+    //
     "sourcecode" : {
         "ru": "Исходный код доступен на GitHub",
         "ua": "Вихідний код доступний на GitHub",
         "en": "Source code available on GitHub",
+        "cz": "Zdrojový kód je k dispozici na GitHub",
+        "be": "Зыходны код даступны на GitHub",
     },
 }
