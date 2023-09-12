a3dfb9826e3b3d6e7146c554818199e83ba8b485
ミーシャ-チェリー <72266338+bublickkk@users.noreply.github.com>
2023-09-12 21:21:18 +0300

    Deleted documents

--- new/doc/js/script.js
+++
@@ -1,5 +0,0 @@
-if (top.location.search=="") {
-    pass = prompt('Введите пароль');
-    if (pass=='testpage050321')
-    { alert('Пароль принят') } else { alert('Пароль непринят!'), top.location.href="../notifications/fail.html" }
-};
